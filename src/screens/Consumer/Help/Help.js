7c54b77953ce5e59d27467f3e6e462a0bc448451
billlyd123 <billlyd123@gmail.com>
2019-02-16 16:45:48 -0800

    fuck martin

--- src/screens/Consumer/Help/Help.js
+++ src/screens/Consumer/Help/Help.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
-import { View, Text, Dimensions, StyleSheet, Platform } from "react-native";
+import { View, Text, Dimensions, StyleSheet, Platform, TextInput } from "react-native";
 import Icon from "react-native-vector-icons/Ionicons";
 
+import DefaultInput from "../../../components/UI/DefaultInput/DefaultInput";
+import ButtonWithBackground from "../../../components/UI/ButtonWithBackground/ButtonWithBackground";
 
+const screenWidth = Dimensions.get("window").width;
+const screenHeight = Dimensions.get('window').height;
 
 class Help extends Component {
     
+
     componentDidMount() {
         Icon.getImageSource(
             Platform === "android" ? "md-chatbubbles" : "md-chatbubbles",
@@ -23,6 +28,7 @@ class Help extends Component {
     constructor(props) {
         super(props);
         this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent);
+        this.state = {text: 'How can we assist you?'};
     }
 
     onNavigatorEvent = event => {
@@ -122,13 +128,90 @@ class Help extends Component {
         }
     };
 
+    submitHelp = () => { alert("Help Submitted!") }
+
     render() {
         return (
-            <View>
-                <Text>Help</Text>
+            <View style={styles.container}>
+                <View style={styles.topContainer}>
+                    <Text style={styles.topText}>
+                        Get help, now 
+                    </Text>
+                    <TextInput
+                        style={[styles.textStyle, styles.textBox]}
+                        onChangeText={(text) => this.setState({text})}
+                        value={this.state.text}
+                        multiline= {true}
+                        onFocus= {() => this.setState({text : ''})}
+                    />
+
+                </View>
+            
+                <View style={styles.inputContainer}>
+                    <ButtonWithBackground style={styles.buttonContainer} textStyle={[styles.buttonText, { color: "white" }]} onPress={this.submitHelp}>
+                        SUBMIT
+                    </ButtonWithBackground>
+                </View>
             </View>
+
         );
     }
 }
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    topText: {
+        fontFamily: "Montserrat-Black",
+        color:"#868686",
+        fontSize: 22,
+        marginLeft: screenWidth * .02
+    },
+    topContainer: {
+        marginTop: screenHeight * .05,
+        marginHorizontal: screenWidth * .1,
+        paddingBottom: screenWidth * .1,
+        alignItems: 'stretch',
+        justifyContent: 'space-around'
+    },
+    textBox: {
+        height: screenHeight * .15,
+        marginTop: screenHeight *.01,
+        borderColor: "#BEBEBE",
+        borderWidth: 2,
+        borderRadius: 7,
+        textAlignVertical: 'top',
+        paddingHorizontal: screenWidth * .03
 
+    },
+    buttonText: { 
+        color: "white", 
+        fontSize: 13, 
+        fontWeight: "400", 
+        fontFamily: "Montserrat-Black",
+        letterSpacing: 1,
+
+    },
+    inputContainer: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center'
+    },
+    buttonContainer: { 
+      backgroundColor: "#7FBCCE" ,
+      height: screenHeight * 0.08,
+      width: screenWidth,
+      borderRadius: 0,
+      borderColor: 'transparent',
+      margin: 0,
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    textStyle: {
+    color: "#868686",
+    fontSize: 14,
+    fontWeight: "500",
+    fontFamily: "Montserrat-Black"
+  }
+});
 export default Help;
